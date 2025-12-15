592dc312b42c37029ccb6434156e37e0b80f9e2a
leilaweicman <leilaweic@hotmail.com>
2025-12-15 11:29:53 +0100

    feat: Add reactive PlayerRepository for MySQL using R2DBC

---
+++ src/main/java/cat/itacademy/s05/blackjack/player/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s05.blackjack.player.repository;
+
+import cat.itacademy.s05.blackjack.domain.player.Player;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
+
+public interface PlayerRepository extends R2dbcRepository<Player, Long> {
+
+    Mono<Boolean> existsByName(String name);
+}
+
