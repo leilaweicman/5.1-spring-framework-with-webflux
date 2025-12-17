d789a32480d51c1c8341e0b244f9302ed9ba5a45
leilaweicman <leilaweic@hotmail.com>
2025-12-17 13:07:51 +0100

    feat: implements get game in GetGameDetailsUseCaseImpl

---
+++ src/main/java/cat/itacademy/s05/blackjack/application/usecase/impl/GetGameDetailsUseCaseImpl.java
@@ -0,0 +1,23 @@
+package cat.itacademy.s05.blackjack.application.usecase.impl;
+
+import cat.itacademy.s05.blackjack.application.usecase.GetGameDetailsUseCase;
+import cat.itacademy.s05.blackjack.domain.exception.GameNotFoundException;
+import cat.itacademy.s05.blackjack.domain.model.aggregates.Game;
+import cat.itacademy.s05.blackjack.domain.repository.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class GetGameDetailsUseCaseImpl implements GetGameDetailsUseCase {
+
+    private final GameRepository gameRepository;
+
+    @Override
+    public Mono<Game> get(String id) {
+        return gameRepository.findById(id)
+                .switchIfEmpty(Mono.error(new GameNotFoundException(id)));
+    }
+}
+
