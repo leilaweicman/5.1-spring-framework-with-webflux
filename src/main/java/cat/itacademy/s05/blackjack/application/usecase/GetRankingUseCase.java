df90c652b44cb6f7b1764ef2ee9acd5cbc2ab332
leilaweicman <leilaweic@hotmail.com>
2025-12-17 13:19:02 +0100

    feat: implements getRanking in GetRankingUseCaseImpl

--- src/main/java/cat/itacademy/s05/blackjack/application/usecase/GetRankingUseCase.java
+++ src/main/java/cat/itacademy/s05/blackjack/application/usecase/GetRankingUseCase.java
@@ -1,8 +1,9 @@
 package cat.itacademy.s05.blackjack.application.usecase;
 
+import cat.itacademy.s05.blackjack.domain.model.aggregates.Player;
 import cat.itacademy.s05.blackjack.domain.model.valueobjects.PlayerRanking;
 import reactor.core.publisher.Flux;
 
 public interface GetRankingUseCase {
-    Flux<PlayerRanking> getRanking();
+    Flux<Player> getRanking();
 }
