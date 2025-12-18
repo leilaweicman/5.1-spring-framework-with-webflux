ace01cd50402322d17451ff702fe9526e096bcf7
leilaweicman <leilaweic@hotmail.com>
2025-12-18 12:27:45 +0100

    refactor: change valueobject and aggregates

--- src/main/java/cat/itacademy/s05/blackjack/domain/model/aggregates/Game.java
+++ src/main/java/cat/itacademy/s05/blackjack/domain/model/aggregates/Game.java
@@ -1,31 +1,20 @@
 package cat.itacademy.s05.blackjack.domain.model.aggregates;
 
-import cat.itacademy.s05.blackjack.domain.model.valueobjects.Deck;
-import cat.itacademy.s05.blackjack.domain.model.valueobjects.GameStatus;
-import cat.itacademy.s05.blackjack.domain.model.valueobjects.Hand;
+import cat.itacademy.s05.blackjack.domain.model.valueobjects.*;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @Builder
-@Document(collection = "games")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Game {
-
-    @Id
-    private String id;
-
-    private Long playerId;
+    private GameId id;
+    private PlayerId playerId;
     private GameStatus status;
-
     private Deck deck;
-
     private Hand playerHand;
     private Hand dealerHand;
 }
-
