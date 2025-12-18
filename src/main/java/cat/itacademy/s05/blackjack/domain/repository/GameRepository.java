36832654a1b798daadba99789042191c450cd55e
leilaweicman <leilaweic@hotmail.com>
2025-12-18 12:41:08 +0100

    refactor(domain): changes in GameEngine, GameRepository and PlayerRepository

--- src/main/java/cat/itacademy/s05/blackjack/domain/repository/GameRepository.java
+++ src/main/java/cat/itacademy/s05/blackjack/domain/repository/GameRepository.java
@@ -1,7 +1,19 @@
 package cat.itacademy.s05.blackjack.domain.repository;
 
 import cat.itacademy.s05.blackjack.domain.model.aggregates.Game;
+import cat.itacademy.s05.blackjack.domain.model.valueobjects.GameId;
+import cat.itacademy.s05.blackjack.domain.model.valueobjects.PlayerId;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-public interface GameRepository extends ReactiveMongoRepository<Game, String> {
+public interface GameRepository {
+
+    Mono<Game> save(Game game);
+
+    Mono<Game> findById(GameId id);
+
+    Flux<Game> findByPlayerId(PlayerId playerId);
+
+    Mono<Void> deleteById(GameId id);
 }
