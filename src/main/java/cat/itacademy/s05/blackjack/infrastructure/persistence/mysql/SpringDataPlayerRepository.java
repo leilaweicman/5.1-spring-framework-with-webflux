28c94082a0b4c2b1bc954095a9eec46c680ceb66
leilaweicman <leilaweic@hotmail.com>
2025-12-23 17:03:38 +0100

    Feature/docker profile (#28)
    * Add docker Spring profile configuration
    
    * feat: limit persistence repositories and adapters to docker profile

--- src/main/java/cat/itacademy/s05/blackjack/infrastructure/persistence/mysql/SpringDataPlayerRepository.java
+++ src/main/java/cat/itacademy/s05/blackjack/infrastructure/persistence/mysql/SpringDataPlayerRepository.java
@@ -1,10 +1,12 @@
 package cat.itacademy.s05.blackjack.infrastructure.persistence.mysql;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
+@Profile("docker")
 public interface SpringDataPlayerRepository extends ReactiveCrudRepository<PlayerEntity, Long> {
 
     Mono<PlayerEntity> findByName(String name);
