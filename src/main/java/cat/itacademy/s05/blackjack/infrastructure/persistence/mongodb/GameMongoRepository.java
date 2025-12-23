89ade65ba42fab1f3578886ad58b0c9a200689da
leilaweicman <leilaweic@hotmail.com>
2025-12-23 17:04:21 +0100

    Dev (#29)
    * Feature/docker profile (#26)
    
    * Feature/docker profile (#28)
    
    * Add docker Spring profile configuration
    
    * feat: limit persistence repositories and adapters to docker profile

--- src/main/java/cat/itacademy/s05/blackjack/infrastructure/persistence/mongodb/GameMongoRepository.java
+++ src/main/java/cat/itacademy/s05/blackjack/infrastructure/persistence/mongodb/GameMongoRepository.java
@@ -1,8 +1,12 @@
 package cat.itacademy.s05.blackjack.infrastructure.persistence.mongodb;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
+@Profile("docker")
 public interface GameMongoRepository extends ReactiveMongoRepository<GameDocument, String> {
 
     Flux<GameDocument> findByPlayerId(Long playerId);
